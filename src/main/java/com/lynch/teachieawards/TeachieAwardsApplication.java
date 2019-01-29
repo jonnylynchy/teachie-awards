95c538130512345f3501f745c7bd01017710eff8
Jon Lynch <jonnylynchy@gmail.com>
2019-01-28 20:54:55 -0600

    initial commit

---
+++ src/main/java/com/lynch/teachieawards/TeachieAwardsApplication.java
@@ -0,0 +1,14 @@
+package com.lynch.teachieawards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachieAwardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachieAwardsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/lynch/teachieawards/TeachieAwardsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lynch.teachieawards;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeachieAwardsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
